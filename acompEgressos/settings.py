53205a6e787e18a46722c54798cff225f35dbfb9
MarceloCRM <marcelo.crmoura@gmail.com>
2026-04-17 08:26:47 -0300

    nova url

--- acompEgressos/settings.py
+++ acompEgressos/settings.py
@@ -50,10 +50,10 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["tccacompegressos-production.up.railway.app", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["acomegressos-production.up.railway.app", "localhost", "127.0.0.1"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://tccacompegressos-production.up.railway.app",
+    "https://acomegressos-production.up.railway.app",
 ]
 # Application definition
 
